2c1cb7283f8c355eccd6cb8b64b2f4bec05ebdf9
t-fowler <52936210+t-fowler@users.noreply.github.com>
2022-08-10 23:29:38 -0700

    Refactor: helper method for checking list table.

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith wants to try out a new, online to-do list app. She goes
         # to check out the home page.
@@ -37,9 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box, inviting her to add another item. She
         # enters "Make peacock feather flies".
@@ -53,10 +56,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, now showing both items on the list.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Make peacock feather flies', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Make peacock feather flies')
 
         # Edith wonders whether the site will remember her list. The
         # page explains that her list has been generated a unique URL.
--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,4 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-from django.template.loader import render_to_string
-
-from lists.views import home_page
 
 # Create your tests here.
 class HomePageTest(TestCase):
