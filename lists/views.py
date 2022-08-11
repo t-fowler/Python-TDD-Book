7352858406a6f795a2026269b464a7c39bc58889
t-fowler <52936210+t-fowler@users.noreply.github.com>
2022-08-11 09:50:56 -0700

    refactor

--- lists/views.py
+++ lists/views.py
@@ -7,7 +7,7 @@ def home_page(request):
     item = Item()
     item.text = request.POST.get('item_text', '')
     item.save()
-    
+
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
+        'new_item_text': item.text,
     })
