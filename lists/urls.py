976de7c0047a92db6ca073c96ffa90da09333bfa
t-fowler <52936210+t-fowler@users.noreply.github.com>
2022-08-12 13:03:51 -0700

    Refactored list application urls into separate file.

--- lists/urls.py
+++ lists/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
-from lists import views as list_views
-from lists import urls as list_urls
+from django.urls import path
+from lists import views
 
 urlpatterns = [
-    path('', list_views.home_page, name="home"),
-    path('lists/', include(list_urls)),
+    path('new', views.new_list, name="new_list"),
+    path('<str:list_id>/', views.view_list, name="view_list"),
+    path('<str:list_id>/add_item', views.add_item, name="add_item")
 ]
