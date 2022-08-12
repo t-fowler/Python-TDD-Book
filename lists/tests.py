d30329ad5e684c6c7ec2fe5c508b4a564aca0faf
t-fowler <52936210+t-fowler@users.noreply.github.com>
2022-08-12 12:58:37 -0700

    Added new POST request URL for adding items to list. Functional Tests now pass.

--- lists/tests.py
+++ lists/tests.py
@@ -61,6 +61,12 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'Other item 1')
         self.assertNotContains(response, 'Other item 2')
 
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
+
 
 class NewListTest(TestCase):
     """Tests the functionality of starting a new list."""
@@ -74,3 +80,31 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+
+class NewItemTest(TestCase):
+    """Tests the functionality of adding a new item to an existing list."""
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list.'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list.')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list.'}
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
