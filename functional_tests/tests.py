d62d58569f581e2737f15fdbf360339036603dc2
t-fowler <52936210+t-fowler@users.noreply.github.com>
2022-08-17 19:16:47 -0700

    Used bootstrap to improve layout.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -7,7 +7,7 @@ import time
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -43,7 +43,7 @@ class NewVisitorTest(LiveServerTestCase):
         input_box = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(
             input_box.get_attribute('placeholder'),
-            'Start a new To-Do list.'
+            'Enter a to-do item'
         )
 
         # She types "Buy peacock feathers" into the text box.
@@ -110,3 +110,28 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element(By.TAG_NAME, 'body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
+
+        
+    def test_layout_and_styling(self):
+        # Edith goes to the home page.
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She sees a nicely centered input box.
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # She starts a new list and the new input is nicely centered too.
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
