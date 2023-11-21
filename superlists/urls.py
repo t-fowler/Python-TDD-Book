3c614ebf0639655f1476b648f0249064f84f8bc5
Tyler Fowler <52936210+t-fowler@users.noreply.github.com>
2023-11-20 21:11:03 -0800

    Switched to regex path.

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import re_path
 from lists import views
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
+    re_path(r'^$', views.home_page, name='home'),
 ]
