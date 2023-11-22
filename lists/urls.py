8484165f63632843732dad0d502ad5782671bc0d
Tyler Fowler <52936210+t-fowler@users.noreply.github.com>
2023-11-21 16:09:24 -0800

    Extracted list view urls into a lists app urls.

--- lists/urls.py
+++ lists/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include, re_path
-from lists import views as list_views
-from lists import urls as list_urls
+from django.urls import re_path
+from lists import views
 
 urlpatterns = [
-    re_path(r'^$', list_views.home_page, name='home'),
-    re_path(r'^lists/', include(list_urls)),
+    re_path(r'^new$', views.new_list, name='new_list'),
+    re_path(r'^(\d+)/$', views.view_list, name='view_list'),
+    re_path(r'^(\d+)/add_item$', views.add_item, name='add_item'),
 ]
