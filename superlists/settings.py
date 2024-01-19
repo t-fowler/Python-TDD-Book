815f6f213736e59361274725a315a6e6b2a617c2
Tyler Fowler <tfowl@proton.me>
2024-01-18 23:50:58 -0800

    Prod now uses DJANGO_DEBUG_FALSE, DJANGO_SECRET_KEY and SITENAME env variables

--- superlists/settings.py
+++ superlists/settings.py
@@ -27,7 +27,7 @@ if 'DJANGO_DEBUG_FALSE' in os.environ:
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
     ALLOWED_HOSTS = [os.environ['SITENAME']]
 else:
-    DEBUG = TRUE
+    DEBUG = True
     SECRET_KEY = 'insecure-key-for-dev'
     ALLOWED_HOSTS = []
 
